c77a65b5134037e7bd978f53fd709bf5aad3ba8e
Papi, Soheib <up.sohaib@hotmail.com>
2023-09-29 10:38:12 +0330

    Project setup.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+IDatabase db = redis.GetDatabase(0);
+
+Console.WriteLine(db.StringGet("user:1").ToString());
+
+
+string value = "abcdefg";
+db.StringSet("mykey", value);
+
+Console.ReadLine();
